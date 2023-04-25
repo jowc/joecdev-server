9205eab575fba0728909456128e18283ab3b396c
Joseph Chikeme <jchikeme@gmail.com>
2023-04-25 18:12:47 +0100

    used async enviroment variables configuration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,32 +1,40 @@
-import { ConfigModuleOptions } from '@nestjs/config';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import {
+  ConfigModule,
+  ConfigModuleOptions,
+  ConfigService,
+} from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
 import { Photo } from 'src/photography/photography.entity';
 import { Portfolio } from 'src/portfolio/entities/portfolio.entity';
 import { Stack } from 'src/portfolio/entities/stack.entity';
 
-export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'joecdev',
-  entities: [Portfolio, Stack, Photo],
-  synchronize: true,
+export class TypeORMConfig {
+  static getConfig(_configService: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: _configService.get('DB_HOST'),
+      port: _configService.get('DB_PORT'),
+      username: _configService.get('DB_USER'),
+      password: _configService.get('DB_PASSWORD'),
+      database: _configService.get('DB'),
+      entities: [Portfolio, Stack, Photo],
+      synchronize: true,
+    };
+  }
+}
+
+export const typeORMConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => TypeORMConfig.getConfig(configService),
 };
 
 export const configModuleOption: ConfigModuleOptions = {
   isGlobal: true,
   envFilePath: '.development.env',
-};
-
-const typeORMConfigEnv: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB,
-  entities: [Portfolio, Stack, Photo],
-  synchronize: true,
 };
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { configModuleOption, typeORMConfig } from 'src/config/typeorm.config';
+import {
+  configModuleOption,
+  typeORMConfigAsync,
+} from 'src/config/typeorm.config';
 import { PhotographyModule } from 'src/photography/photography.module';
 import { PortfolioModule } from 'src/portfolio/portfolio.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configModuleOption),
-    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forRootAsync(typeORMConfigAsync),
     PortfolioModule,
     PhotographyModule,
   ],
