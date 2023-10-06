42a22bd43b4869e818c71b525985816cd3b63c5e
Joseph Chikeme <jchikeme@gmail.com>
2023-10-06 15:06:16 +0100

    added security

---
+++ src/config/throttle.config.ts
@@ -0,0 +1,13 @@
+import { ConfigService } from '@nestjs/config';
+import { ThrottlerModuleOptions } from '@nestjs/throttler';
+
+export class ThrottleConfig {
+  getConfig(): ThrottlerModuleOptions {
+    return [
+      {
+        ttl: 60000,
+        limit: 20,
+      },
+    ];
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+import helmet from 'helmet';
 import { SwaggerModule } from '@nestjs/swagger';
 
 import { config as swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: ['localhost:3000', 'https://joecdev.com'] });
+  app.use(helmet());
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger-api', app, document);
 
